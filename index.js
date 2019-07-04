d4608e511c523da0e6202501fcd7ca1b38b11356
Miguel Benavides <tecnologiapresente@gmail.com>
2019-07-04 10:55:31 -0400

    index fixed

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const Microservices = require('./lib/Microservices');
 const Messages = require('./lib/Messages');
-const Monitor = require('./lib/Monitor');
 
 module.exports.Microservices = Microservices;
-module.exports.Messages = Messages;
