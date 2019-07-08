458be68110f7caeee5b7a3bd0b38605bb67bd480
Miguel Benavides <tecnologiapresente@gmail.com>
2019-07-07 21:52:36 -0400

    EventStorage

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Microservices = require('./lib/Microservices');
 const Messages = require('./lib/Messages');
+const Events = require('./lib/Events');
 
 module.exports.Microservices = new Microservices();
+module.exports.Events = Events;
