a6f938c128dbe9423cf66f99134a43e1f7d1175b
Miguel Benavides <tecnologiapresente@gmail.com>
2019-07-08 14:18:20 -0400

    Redesign of Events. Now they are Inbox

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const Microservices = require('./lib/Microservices');
 const Messages = require('./lib/Messages');
-const Events = require('./lib/Events');
 
 module.exports.Microservices = new Microservices();
-module.exports.Events = Events;
