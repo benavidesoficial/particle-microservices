c7f098ef7f4ff288baf73cf078e1746c4f8688c3
Miguel Benavides <tecnologiapresente@gmail.com>
2019-09-10 12:18:01 -0400

    import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const root = require('app-root-path');
-const Microservices = require(`${root}/lib/Microservices`);
-const Messages = require(`${root}/lib/Messages`);
+const Microservices = require(`./lib/Microservices`);
+const Messages = require(`./lib/Messages`);
 
 module.exports.Microservices = new Microservices();
